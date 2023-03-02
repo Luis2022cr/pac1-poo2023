9cf5adc62981643b8a1f0efc657522fc5890bc5e
Luis2022cr <joseluiscrz129@gmail.com>
2023-03-01 22:34:39 -0600

    Portafolio

---
+++ unidad2/Portafolio/Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //ViewBag.Nombre = "Jose Luis Contreras";
+            //ViewBag.Edad = "21";
+            var persona = new Persona()
+            {
+                Nombre = "Jose Luis Contreras",
+                Edad = 21
+            };
+
+            return View("index", persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
